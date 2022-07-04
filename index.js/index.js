da3f3bc45bed82eb2b9ac88097df992d421fa2ea
Komilov Khayrulla <108469321+Komilov21@users.noreply.github.com>
2022-07-04 16:19:36 +0200

    Files added

---
+++ index.js/index.js
@@ -0,0 +1,22 @@
+let myFirstname="Khayrulla"
+
+let myLastname="Komilov"
+
+let myMiddlename="Bayjonovich"
+
+
+let FullName
+FullName =myFirstname +" " +myLastname + " "+myMiddlename;
+
+console.log("Hello,my fullname is", FullName);
+
+let age = 19
+let currentyear=2022
+
+let myBirthyear
+myBirthyear= currentyear-age
+
+console.log("My birth year is ", myBirthyear)
+
+
+
